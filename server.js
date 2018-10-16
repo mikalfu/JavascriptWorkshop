c3b3b9a90012bdaa3a015d0fab5450dcf045df9f
Mikal Fuglestein <mikal.fuglestein@student.uib.no>
2018-10-16 18:55:07 +0200

    morgan

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+app.get('/', (request, response) => response.send('Hello Machine'));
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
+});
